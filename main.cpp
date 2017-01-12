ecca3368a41f2309bbbd6ef91e4526f0311104fa
zimo <nazim.coindet@gmail.com>
2017-01-12 16:32:24 -0500

    for loop modified

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@ using namespace std;
 int main()
 {
 
- cout<<"hello word!"<<endl;
+	int N =10;
+	 cout<<"hello word!"<<endl;
 
-	for(int i =0;i=10;i++)
+	for(int i =0;i=N;i++)
 	{
 		cout<<i<<endl;
 	}	
